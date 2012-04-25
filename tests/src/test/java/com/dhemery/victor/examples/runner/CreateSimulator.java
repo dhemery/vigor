927ccb41fa652745cd9fbeaf06250bbf9cb03e6e
Dale Emery <dale@dhemery.com>
2012-04-24 22:53:21 -0700

    Get default developer root from xcode-select.

--- tests/src/test/java/com/dhemery/victor/examples/runner/CreateSimulator.java
+++ tests/src/test/java/com/dhemery/victor/examples/runner/CreateSimulator.java
@@ -2,20 +2,26 @@ package com.dhemery.victor.examples.runner;
 
 import com.dhemery.victor.device.LocalSimulator;
 import com.dhemery.victor.device.Simulator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Properties;
 
 public class CreateSimulator {
-    public static final String DEFAULT_DEVELOPER_ROOT = "/Applications/Xcode.app/Contents/Developer";
-    public static final String DEFAULT_SDK_ROOT_TEMPLATE = "%s/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator%s.sdk";
+    public static final String DEFAULT_SDK_ROOT_FOR_DEVELOPER_ROOT_AND_SDK_VERSION = "%s/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator%s.sdk";
     public static final String DEFAULT_SDK_VERSION = "5.1";
-    public static final String DEFAULT_SIMULATOR_BINARY_PATH_TEMPLATE = "%s/Platforms/iPhoneSimulator.platform/Developer/Applications/iPhone Simulator.app/Contents/MacOS/iPhone Simulator";
+    public static final String DEFAULT_SIMULATOR_BINARY_PATH_FOR_DEVELOPER_ROOT = "%s/Platforms/iPhoneSimulator.platform/Developer/Applications/iPhone Simulator.app/Contents/MacOS/iPhone Simulator";
 
     public static final String DEVELOPER_ROOT_PROPERTY = "victor.developer.root";
     public static final String SDK_ROOT_PROPERTY = "victor.sdk.root";
     public static final String SDK_VERSION_PROPERTY = "victor.sdk.version";
     public static final String SIMULATOR_BINARY_PATH_PROPERTY = "victor.simulator.binary.path";
 
+    private final Logger log = LoggerFactory.getLogger(getClass());
     private final Properties properties;
 
     public static Simulator fromProperties(Properties properties) {
@@ -42,7 +48,9 @@ public class CreateSimulator {
     }
 
     private Simulator create() {
-        return new LocalSimulator(sdkRoot(), simulatorBinaryPath());
+        String sdkRoot = sdkRoot();
+        log.info("sdk root {} ", sdkRoot);
+        return new LocalSimulator(sdkRoot, simulatorBinaryPath());
     }
 
     private String developerRoot() {
@@ -64,14 +72,25 @@ public class CreateSimulator {
 
 
 
-
-    // todo use xcode-switch -print-path
     private String defaultDeveloperRoot() {
-        return DEFAULT_DEVELOPER_ROOT;
+        try {
+            Process process = new ProcessBuilder().command("xcode-select", "-print-path").start();
+            return outputFromProcess(process);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
+    private String outputFromProcess(Process process) throws IOException {
+        InputStream inputStream = process.getInputStream();
+        InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+        return bufferedReader.readLine();
+    }
+
+    // todo Use SDKROOT if specified.
     private String defaultSdkRoot() {
-        return String.format(DEFAULT_SDK_ROOT_TEMPLATE, developerRoot(), sdkVersion());
+        return String.format(DEFAULT_SDK_ROOT_FOR_DEVELOPER_ROOT_AND_SDK_VERSION, developerRoot(), sdkVersion());
     }
 
     // todo Default to the latest installed SDK.
@@ -80,6 +99,6 @@ public class CreateSimulator {
     }
 
     private String defaultSimulatorBinaryPath() {
-        return String.format(DEFAULT_SIMULATOR_BINARY_PATH_TEMPLATE, developerRoot());
+        return String.format(DEFAULT_SIMULATOR_BINARY_PATH_FOR_DEVELOPER_ROOT, developerRoot());
     }
 }
