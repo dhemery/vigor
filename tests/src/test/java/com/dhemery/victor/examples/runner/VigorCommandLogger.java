2a4dd19a0d689a56fae11345fbcc796de7a178ea
Dale Emery <dale@dhemery.com>
2013-10-23 00:59:25 -0700

    Use Hartley's new, cleaner command code

--- tests/src/test/java/com/dhemery/victor/examples/runner/VigorCommandLogger.java
+++ tests/src/test/java/com/dhemery/victor/examples/runner/VigorCommandLogger.java
@@ -1,6 +1,6 @@
 package com.dhemery.victor.examples.runner;
 
-import com.dhemery.os.OSCommand;
+import com.dhemery.os.Command;
 import com.dhemery.os.events.Returned;
 import com.dhemery.os.events.WillRun;
 import com.dhemery.publishing.Subscribe;
@@ -12,13 +12,13 @@ public class VigorCommandLogger {
 
     @Subscribe
     public void willRun(WillRun event) {
-        OSCommand command = event.command();
+        Command command = event.command();
         log.info("{} --> {} {}", new Object[]{ command.description(), command.path(), command.arguments()});
     }
 
     @Subscribe
     public void returned(Returned event) {
-        OSCommand command = event.command();
+        Command command = event.command();
         log.info("{} <-- {}", command.description(), event.output());
     }
 }
